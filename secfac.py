19fdd90676220f13e628834b48bdb0be1d8ba584
Raveline <eraveline@gmail.com>
2014-06-12 23:29:21 +0200

    Fixed menu messaging on recruitment.

--- secfac.py
+++ secfac.py
@@ -35,11 +35,12 @@ class MenuItem(object):
     ITEM_COMPLEMENT = 1     # This item will carry a complement constant
     ITEM_NONE = 2           # This item carry no information, just children
     def __init__(self, label, shortcut, message_type,
-                message_part, children = []):
+                message_part, explaining_text = None, children = []):
         self.label = label
         self.message_type = message_type
         self.message_part = message_part
         self.children = children
+        self.explaining_text = explaining_text
         self.shortcut = shortcut
 
 class FacilityMap(Focusable):
@@ -66,7 +67,7 @@ class FacilityMap(Focusable):
     def pressedOn(self, x, y):
         (x,y) = self.screen.local_to_global(x,y)
         if self.currentAction is not Message.VIEW:
-            self.extendSelectionTo(x,y)
+            self.startSelection(x,y)
         else:
             self.screen.move_center(x,y)
 
@@ -92,6 +93,7 @@ class FacilityMap(Focusable):
     def escape(self):
         if self.pane.can_go_back():
             self.pane.go_back()
+            self.change_menu_action(self.pane.current_branch)
         else:
             messages.receive(Message(Message.QUIT))
 
@@ -101,19 +103,18 @@ class FacilityMap(Focusable):
         else:
             self.screen.show_pane()
 
+    def change_menu_action(self, submenu):
+        if submenu.message_type == MenuItem.ITEM_VERB:
+            self.currentAction = submenu.message_part
+            self.currentComplement = None
+        if submenu.message_type == MenuItem.ITEM_COMPLEMENT:
+            self.currentComplement = submenu.message_part
+
     def append_char(self, char):
         submenu = self.pane.shortcuts.get(char, None)
         if submenu is not None:
-            if submenu.message_type == MenuItem.ITEM_VERB:
-                self.currentAction = submenu.message_part
-                self.currentComplement = None
-            if submenu.message_type == MenuItem.ITEM_COMPLEMENT:
-                # Ideally, here, we should divide between verbs with area
-                # which will require some more work before sending message
-                # and verbs without area, which will work now.
-                if self.currentAction in Message.area_verbs:
-                    self.currentComplement = submenu.message_part
-            if len(submenu.children):
+            self.change_menu_action(submenu)
+            if submenu.children or submenu.explaining_text:
                 self.pane.enter_branch(submenu)
             else:
                 messages.receive(Message(self.currentAction, self.currentComplement))
@@ -341,11 +342,14 @@ if __name__ == "__main__":
     facility = buildFacility()
 
     # TODO : read all that from a config file
-    tree = MenuItem("Main menu", '', MenuItem.ITEM_VERB, Message.VIEW, [
-                MenuItem("(D)ig", 'd', MenuItem.ITEM_VERB, Message.DIG),
-                MenuItem("(B)uild", 'b', MenuItem.ITEM_VERB, Message.BUILD, [
+    tree = MenuItem("Main menu", '', MenuItem.ITEM_VERB, Message.VIEW, "", [
+                MenuItem("(D)ig", 'd', MenuItem.ITEM_VERB, Message.DIG,
+                        "Select tiles to dig."),
+                MenuItem("(B)uild", 'b', MenuItem.ITEM_VERB
+                    , Message.BUILD, "", [
                     MenuItem("(E)levator", 'e', MenuItem.ITEM_COMPLEMENT, "")]),
-                MenuItem("(R)ecruit", 'r', MenuItem.ITEM_VERB, Message.RECRUIT, [
+                MenuItem("(R)ecruit", 'r', MenuItem.ITEM_VERB,
+                        Message.RECRUIT, "", [
                     MenuItem("(W)orker", 'w', MenuItem.ITEM_COMPLEMENT,
                                         EmployeeType.WORKER),
                     MenuItem("(S)ecurity guard", 's', MenuItem.ITEM_COMPLEMENT,
